0381017fe4131a10ae625c8cb0a675d6e0cc40a7
asatvoong <asatvoong@hotmail.com>
2018-12-16 17:17:42 +0100

    Robots app have cards now
    Robots app have cards now.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App.js';
-import Hello from './Hello.js';
+import Card from './Card.js';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import robots from './robots.js'
 
-ReactDOM.render(<Hello dateValue = {'Today is ' + Date.now()} />, document.getElementById('root'));
+ReactDOM.render(
+	<div>
+		<Card />
+		<Card />
+		<Card />
+	</div>
+	, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
